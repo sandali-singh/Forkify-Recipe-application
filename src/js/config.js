998f1328d6c8e8e0016e0435b709a658d75951e9
sandali-singh <sandalisingh1270@gmail.com>
2023-12-05 21:07:32 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '18465502-73db-4037-a184-bcdbc2fc8aa4';
+export const MODAL_CLOSE_SEC = 2.5;
